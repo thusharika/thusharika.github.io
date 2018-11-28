9648e761cc828475afdbc43ba22fb6c2291300fc
Thusharika <thusharikan@gmail.com>
2018-11-27 22:46:48 -0500

    portfolio

---
+++ precache-manifest.4476d6834b5b85dcd48e2e92946f0019.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5ef8c6b1137bdfbca0c",
+    "url": "/static/js/main.d5ef8c6b.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "d5ef8c6b1137bdfbca0c",
+    "url": "/static/css/main.4d0db8bd.chunk.css"
+  },
+  {
+    "revision": "912b3cc606d6e3a249751e45a5767af3",
+    "url": "/index.html"
+  }
+];
